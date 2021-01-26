0b40af6b8d99a900df1c4cd298d6b0b908839ed6
Quanzer <57160472+Quanzer@users.noreply.github.com>
2021-01-25 16:12:59 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+// Author: Quan Nguyen and Amin Parvizi
+// Description: Hello World in C++
+//============================================================
+#include <iostream>
+
+using namespace std;
+int hello1Function(string name1);
+int hello2Function(string name2);
+
+int main()
+{
+string name1="Quan Nguyen";
+
+hello1Function(name1);
+
+string name2="Amin Parvizi";
+
+hello2Function(name2);
+
+return 0;
+}
